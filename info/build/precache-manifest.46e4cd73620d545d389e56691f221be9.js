72d99925434b62743cd7101af5475311043d599a
Vadim <asikpro13@mail.ru>
2020-12-24 03:59:38 +0500

    beta v1.1

---
+++ info/build/precache-manifest.46e4cd73620d545d389e56691f221be9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86fd120e402d1f8d7d5e88896f30bd13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba3d74ba889f5194e67c",
+    "url": "/static/css/main.1907675d.chunk.css"
+  },
+  {
+    "revision": "97f14f80d468ce26c643",
+    "url": "/static/js/2.5929e91e.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.5929e91e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba3d74ba889f5194e67c",
+    "url": "/static/js/main.dea6b088.chunk.js"
+  },
+  {
+    "revision": "db3b2b3e28929fe1eb6e",
+    "url": "/static/js/runtime-main.febde9d1.js"
+  }
+]);
